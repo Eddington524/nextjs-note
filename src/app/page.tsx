886ed85be4b73ea0b6ed9f4d13dc6b4064272426
Sammy Jung <sammyjung@Sammyui-MacBookAir.local>
2023-05-10 15:50:25 +0900

    ver2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,19 @@ import os from 'os'; // 노드 APIs
 import Counter from '@/components/Counter';
 
 export default function Home() {
-  console.log('안녕! - 서버');
-  console.log(os.hostname());
+	console.log('안녕! - 서버');
+	console.log(os.hostname());
 
-  return (
-    <>
-      <h1>홈페이지다!!</h1>
-      <Counter />
-      <Image
-        src='https://images.unsplash.com/photo-1441986300917-64674bd600d8'
-        alt='shop'
-        width={400}
-        height={400}
-      />
-    </>
-  );
+	return (
+		<>
+			<h1>홈페이지다!! 버전2</h1>
+			<Counter />
+			<Image
+				src="https://images.unsplash.com/photo-1441986300917-64674bd600d8"
+				alt="shop"
+				width={400}
+				height={400}
+			/>
+		</>
+	);
 }
